7e1290a94aabb0cd7467204139a62329ca137798
Nathan Relyea <relyean19@up.edu>
2019-10-25 12:53:25 -0700

    Second Form & Filling Screen Using "Dock"

--- Rain/Rain/Form2.cs
+++ Rain/Rain/Form2.cs
@@ -10,24 +10,22 @@ using System.Windows.Forms;
 
 namespace Rain
 {
-    public partial class Form1 : Form
+    public partial class Form2 : Form
     {
-        public Form1()
+        public Form2()
         {
             InitializeComponent();
             WindowState = FormWindowState.Maximized;
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void Form2_Load(object sender, EventArgs e)
         {
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void label1_Click(object sender, EventArgs e)
         {
-            Form2 form2 = new Form2();
-            form2.Show();
-            this.Hide();
+
         }
     }
 }
